e9e450909229f1c1a873ec33aa3bab77ac71602b
Robert Plant <rob@robertplant.io>
2021-01-02 01:54:05 +0000

    improved readability of layout

--- ergodox/keymap.c
+++ ergodox/keymap.c
@@ -19,9 +19,9 @@
 
 enum custom_keycodes {
   RGB_SLD = EZ_SAFE_RANGE,
-  ST_MACRO_GO_ERR,
-  ST_MACRO_JS_DEBUG,
-  ST_MACRO_WPM
+  STM_GO_ERR,
+  STM_JS_DEBUG,
+  STM_WPM
 };
 
 char wpm[16];
@@ -29,59 +29,59 @@ char wpm[16];
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_ergodox_pretty(
     KC_ESCAPE,      KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_DELETE,                                      KC_F5,          KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_BSPACE,
-    KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_TRANSPARENT,                                 KC_HASH,        KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_TRANSPARENT,
+    KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           _______,                                        KC_NONUS_HASH,  KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           _______,       
     KC_LGUI,        KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                                                           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCOLON,      KC_QUOTE,
-    KC_LSHIFT,      KC_NONUS_BSLASH,KC_Z,           KC_X,           KC_C,           KC_V,           KC_TRANSPARENT,                                 KC_SCOLON,      KC_B,           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,
-    KC_LCTRL,       MO(3),          MO(2),          MO(1),          KC_LALT,                                                                                                        MO(2),          MO(1),          KC_LBRACKET,    KC_RBRACKET,    KC_RCTRL,
-                                                                                                    KC_TRANSPARENT, KC_TRANSPARENT, KC_MEDIA_PLAY_PAUSE,KC_MEDIA_NEXT_TRACK,
+    KC_LSHIFT,      KC_NONUS_BSLASH,KC_Z,           KC_X,           KC_C,           KC_V,           _______,                                        KC_SCOLON,      KC_B,           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,
+    KC_LCTRL,       MO(3),          MO(2),          MO(1),          KC_LALT,                                                                                                        MO(2),          KC_UNDS,        KC_PMNS,        KC_EQUAL,        KC_RCTRL,
+                                                                                                    _______,        _______,        KC_MPLY,        KC_MNXT,
                                                                                                                     KC_HOME,        KC_PGUP,
                                                                                     KC_SPACE,       KC_BSPACE,      KC_END,         KC_PGDOWN,      KC_TAB,         KC_ENTER
   ),
   [1] = LAYOUT_ergodox_pretty(
-    KC_ESCAPE,      KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_F6,          KC_F7,          KC_F8,          KC_MINUS,       KC_EQUAL,       KC_DELETE,
-    KC_TRANSPARENT, KC_EXLM,        KC_AT,          KC_LCBR,        KC_RCBR,        KC_PIPE,        KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_7,           KC_8,           KC_9,           KC_ASTR,        KC_F12,
-    KC_TRANSPARENT, KC_HASH,        KC_DLR,         KC_LPRN,        KC_RPRN,        KC_GRAVE,                                                                       KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT,       KC_PLUS,        KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_PERC,        KC_CIRC,        KC_LBRACKET,    KC_RBRACKET,    KC_TILD,        KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_AMPR,        KC_1,           KC_2,           KC_3,           KC_BSLASH,      KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_EQUAL,       KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                                                                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_0,           KC_EQUAL,       KC_TRANSPARENT,
-                                                                                                    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-                                                                                                                    KC_TRANSPARENT, KC_TRANSPARENT,
-                                                                                    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT
+    KC_ESCAPE,      KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          _______,                                        _______,        KC_F6,          KC_F7,          KC_F8,          KC_MINUS,       KC_EQUAL,       KC_DELETE,
+    _______,        KC_EXLM,        KC_AT,          KC_LCBR,        KC_RCBR,        KC_PIPE,        _______,                                        _______,        _______,        _______,        _______,        _______,        KC_ASTR,        _______,
+    _______,        KC_HASH,        KC_DLR,         KC_LPRN,        KC_RPRN,        KC_GRAVE,                                                                       KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT,       KC_PLUS,        _______,       
+    _______,        KC_PERC,        KC_CIRC,        KC_LBRACKET,    KC_RBRACKET,    KC_TILD,        _______,                                        _______,        KC_AMPR,        _______,        _______,        _______,        KC_BSLASH,      _______,       
+    _______,        KC_EQUAL,       _______,        _______,        _______,                                                                                                        _______,        _______,        _______,        _______,       _______,       
+                                                                                                    _______,        _______,        _______,        _______,       
+                                                                                                                    _______,        _______,       
+                                                                                    _______,        _______,        _______,        _______,        _______,        _______
   ),
   [2] = LAYOUT_ergodox_pretty(
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_MS_WH_UP,    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_MS_UP,       KC_MS_WH_DOWN,  KC_TRANSPARENT, KC_TRANSPARENT, RESET,                                          KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_PLUS,        KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_MS_LEFT,     KC_MS_DOWN,     KC_MS_RIGHT,    KC_TRANSPARENT, KC_TRANSPARENT,                                                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_MEDIA_PLAY_PAUSE,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_MEDIA_PREV_TRACK,KC_MEDIA_NEXT_TRACK,KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_MS_BTN1,     KC_MS_BTN2,                                                                                                     KC_TRANSPARENT, KC_AUDIO_VOL_DOWN,KC_AUDIO_MUTE,  KC_TRANSPARENT, KC_TRANSPARENT,
-                                                                                                    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-                                                                                                                    KC_TRANSPARENT, KC_TRANSPARENT,
-                                                                                    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_WWW_BACK
+    _______,        _______,        _______,        KC_MS_WH_UP,    _______,        _______,        _______,                                        _______,        _______,        _______,        _______,        _______,        _______,        KC_DELETE,       
+    _______,        _______,        KC_MS_UP,       KC_MS_WH_DOWN,  _______,        _______,        RESET,                                          _______,        _______,        KC_7,           KC_8,           KC_8,           _______,        _______,       
+    _______,        KC_MS_LEFT,     KC_MS_DOWN,     KC_MS_RIGHT,    _______,        _______,                                                                        _______,        KC_4,           KC_5,           KC_6,           _______,        _______,
+    _______,        _______,        _______,        _______,        _______,        _______,        _______,                                        _______,        _______,        KC_1,           KC_2,           KC_3,           _______,        _______,       
+    _______,        _______,        _______,        KC_MS_BTN1,     KC_MS_BTN2,                                                                                                     KC_0,           _______,        _______,        _______,        _______,       
+                                                                                                    _______,        _______,        _______,        _______,
+                                                                                                                    _______,        _______,       
+                                                                                    _______,        _______,        _______,        _______,        _______,        KC_WWW_BACK
   ),
   [3] = LAYOUT_ergodox_pretty(
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, ST_MACRO_GO_ERR,ST_MACRO_WPM,   KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, ST_MACRO_JS_DEBUG,  KC_TRANSPARENT, KC_TRANSPARENT,                                                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                                                                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-                                                                                                    KC_TRANSPARENT, KC_TRANSPARENT, RESET,          KC_TRANSPARENT,
-                                                                                                                    KC_TRANSPARENT, KC_TRANSPARENT,
-                                                                                    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT
+    _______,        _______,        _______,        _______,        _______,        _______,        _______,                                        _______,        _______,        _______,        _______,        _______,        _______,        _______,       
+    _______,        STM_GO_ERR,     STM_WPM,        _______,        _______,        _______,        _______,                                        _______,        _______,        _______,        _______,        _______,        _______,        _______,       
+    _______,        _______,        _______,        STM_JS_DEBUG,   _______,        _______,                                                                        _______,        _______,        _______,        _______,        _______,        _______,       
+    _______,        _______,        _______,        _______,        _______,        _______,        _______,                                        _______,        _______,        _______,        _______,        _______,        _______,        _______,       
+    _______,        _______,        _______,        _______,        _______,                                                                                                        _______,        _______,        _______,        _______,        _______,       
+                                                                                                    _______,        _______,        RESET,          _______,       
+                                                                                                                    _______,        _______,       
+                                                                                    _______,        _______,        _______,        _______,        _______,        _______
   ),
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-    case ST_MACRO_GO_ERR:
+    case STM_GO_ERR:
 		if (record->event.pressed) {
 			SEND_STRING("if err != nil {\n\t\n}" SS_TAP(X_UP));
 		}
 		break;
-    case ST_MACRO_JS_DEBUG:
+    case STM_JS_DEBUG:
 		if (record->event.pressed) {
 			SEND_STRING("^ywoconsole.log('" SS_TAP(X_ESCAPE) "pa');" SS_TAP(X_ESCAPE));
 		}
 		break;
-	case ST_MACRO_WPM:
+	case STM_WPM:
 		if (record->event.pressed) {
             sprintf(wpm, "%03d", get_current_wpm());
 			send_string(wpm);
